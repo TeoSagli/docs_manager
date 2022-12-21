ca64c040244d6ef17cc62a046a09f6ffb0ee1783
Matteo Saglimbeni <79706196+TeoSagli@users.noreply.github.com>
2022-12-21 21:36:55 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/TeoSagli/docs_manager""
    This reverts commit 00bc2578d3ee29c4e3eb8893747628d0b3651e03.

--- lib/frontend/pages/favourites.dart
+++ lib/frontend/pages/favourites.dart
@@ -1,71 +1,117 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
+import '../../backend/delete_db.dart';
+import '../../backend/read_db.dart';
+import '../../backend/update_db.dart';
 import '../components/app_bar.dart';
 import '../components/bottom_bar.dart';
 import '../components/file_card.dart';
 
-class FavouritesPage extends StatelessWidget {
+import 'package:docs_manager/others/constants.dart' as constants;
+
+class FavouritesPage extends StatefulWidget {
   const FavouritesPage({super.key});
 
   @override
+  State<StatefulWidget> createState() => FavouriteViewPageState();
+}
+
+class FavouriteViewPageState extends State<FavouritesPage> {
+  late StreamSubscription readCards;
+  List<Widget> cardsList = [];
+//===================================================================================
+// Activate listeners
+  @override
+  void initState() {
+    setState(() {
+      //readCards = retrieveFilesDB();
+      readCards = retrieveAllFavouriteFilesDB(
+          fulfillCard, moveToFile, moveToEditFile, removeCard);
+    });
+    super.initState();
+  }
+
+//===================================================================================
+// Deactivate listeners
+  @override
+  void deactivate() {
+    readCards.cancel();
+    super.deactivate();
+  }
+
+//===================================================================================
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar("Favourites", false, context),
       bottomNavigationBar: MyBottomBar(context, 3),
-      body: Column(mainAxisSize: MainAxisSize.max, children: [
-        Padding(
-          padding: const EdgeInsetsDirectional.fromSTEB(16, 8, 16, 8),
-          child: Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: const [
-                BoxShadow(
-                  blurRadius: 8,
-                  color: Color(0x230F1113),
-                  offset: Offset(0, 4),
-                )
-              ],
-              borderRadius: BorderRadius.circular(12),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsetsDirectional.fromSTEB(16, 8, 16, 8),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: const [
-                BoxShadow(
-                  blurRadius: 8,
-                  color: Color(0x230F1113),
-                  offset: Offset(0, 4),
-                )
-              ],
-              borderRadius: BorderRadius.circular(12),
-              shape: BoxShape.rectangle,
-            ),
-          ),
-        ),
-        Wrap(
-          spacing: 8,
-          runSpacing: 4,
-          alignment: WrapAlignment.start,
-          crossAxisAlignment: WrapCrossAlignment.start,
-          direction: Axis.horizontal,
-          runAlignment: WrapAlignment.start,
-          verticalDirection: VerticalDirection.down,
-          clipBehavior: Clip.none,
-          children: [],
-        ),
-      ]),
+      appBar: MyAppBar("Favourites", false, context),
+      body: Stack(
+        children: [
+          cardsList.isEmpty
+              ? constants.emptyPage
+              : SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Wrap(
+                      spacing: 8,
+                      runSpacing: 12,
+                      alignment: WrapAlignment.spaceEvenly,
+                      crossAxisAlignment: WrapCrossAlignment.start,
+                      direction: Axis.horizontal,
+                      runAlignment: WrapAlignment.start,
+                      verticalDirection: VerticalDirection.down,
+                      children: cardsList),
+                ),
+        ],
+      ),
     );
   }
 
+//===================================================================================
+// Move to file page
   moveToFile(id, context) {
     Navigator.pushNamed(
       context,
       '/files/view/$id',
     );
+  }
+
+//========================================================
+//Fill file card
+  fulfillCard(
+    List<Widget> myCards,
+  ) {
+    setState(() {
+      cardsList = myCards;
+    });
+    /*print("Cardlist ${cardsList.toList().toString()} is here");
+    print("Orderlist ${itemsList.toList().toString()} is here");*/
+  }
+
+//===================================================================================
+//Move router to Category View page
+  moveToEditFile(fileName, context) {
+    Navigator.pushNamed(
+      context,
+      '/files/edit/$fileName',
+    );
+  }
+
+//========================================================
+//Move router to Category View page
+  removeCard(FileCard cardToDelete) {
+    for (var element in cardsList) {
+      if (element == cardToDelete) {
+        deleteFileDB(cardToDelete.file.categoryName, cardToDelete.fileName);
+        deleteFileStorage(cardToDelete.file.path[0] as String,
+            cardToDelete.file.categoryName);
+        onUpdateNFiles(cardToDelete.file.categoryName);
+        setState(() {
+          cardsList.remove(element);
+        });
+        break;
+      }
+    }
   }
 }
