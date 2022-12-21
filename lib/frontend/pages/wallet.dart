248f5e4a8b8d6dc9941dcba8c2025d6efb602660
Riccardo <riccardo.chioetto@yahoo.com>
2022-12-21 09:45:50 +0100

    Wallet page

--- lib/frontend/pages/wallet.dart
+++ lib/frontend/pages/wallet.dart
@@ -9,26 +9,40 @@ class WalletPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar("Wallet", false, context),
-      bottomNavigationBar: MyBottomBar(context, 1),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Container(
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              color: Color(0xFFF1F4F8),
+        appBar: MyAppBar("Wallet", false, context),
+        bottomNavigationBar: MyBottomBar(context, 1),
+        body: SafeArea(
+          child: GestureDetector(
+            onTap: () => FocusScope.of(context).unfocus(),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Container(
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFF1F4F8),
+                  ),
+                ),
+                WalletCard(
+                    'Tesla Model Y',
+                    'Car',
+                    '1969-07-20',
+                    'assets/images/test.png',
+                    Icons.fitness_center,
+                    0,
+                    moveToFile),
+                WalletCard(
+                    'The Running Ragamuffins',
+                    'Fitness',
+                    '2001-09-20',
+                    'assets/images/test.png',
+                    Icons.fitness_center,
+                    1,
+                    moveToFile)
+              ],
             ),
           ),
-          WalletCard('The Running Ragamuffins', 'Fitness', '1969-07-20',
-              'assets/images/test.png', Icons.fitness_center, 0, moveToFile),
-          WalletCard('The Running Ragamuffins', 'Fitness', '1969-07-20',
-              'assets/images/test.png', Icons.fitness_center, 1, moveToFile),
-          WalletCard('The Running Ragamuffins', 'Fitness', '1969-07-20',
-              'assets/images/test.png', Icons.fitness_center, 2, moveToFile)
-        ],
-      ),
-    );
+        ));
   }
 
   moveToFile(id, context) {
